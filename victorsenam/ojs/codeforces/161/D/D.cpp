574a354875686ca732cb2f35f6312d61441604d8
Victor Sena Molero <victorsenam@gmail.com>
2015-03-09 21:21:08 -0300

    Epic FAIL

--- victorsenam/ojs/codeforces/161/D/D.cpp
+++ victorsenam/ojs/codeforces/161/D/D.cpp
@@ -8,42 +8,36 @@ using namespace std;
 unsigned int d[N][K+1];
 vector<unsigned int> g[N];
 bool s[N];
-int n, k, a, b, root;
+int n, k, a, b, root, p;
 
 long long int  contaPares (int v) {
     long long int r = 0;
     if (d[v][0]) return 0;
     d[v][0] = 1;
+    s[v] = 1;
     
     for (vector<unsigned int>::iterator it = g[v].begin(); it != g[v].end(); it++) {
         if (d[*it][0]) continue;
         r += contaPares(*it);
     }
 
-    d[v][0] = 0;
-
     for (vector<unsigned int>::iterator it = g[v].begin(); it != g[v].end(); it++) {
-        if (d[*it][0]) continue;
-        d[*it][0] = 1;
+        if (s[*it]) continue;
 
-        printf("%d : entra %d\n", v+1, *it+1);
+        for (int j = 0; j < k && d[*it][j]; j++) d[v][j+1] += d[*it][j];
 
-        for (int j = 0; j < k; j++) d[v][j+1] += d[*it][j];
-        for (vector<unsigned int>::iterator jt = it + 1; jt != g[v].end(); jt++) {
-            if (d[*jt][0]) continue;
+        for (int l = 0; l <= k-2; l++) {
+            for (vector<unsigned int>::iterator jt = it + 1; jt != g[v].end(); jt++) {
+                if (s[*jt]) continue;
 
-            for (int l = 0; l <= k-2; l++) {
                 r += d[*it][l] * d[*jt][k-2-l];
             }
-            printf("%d : (%d,%d) => r = %lld\n", v+1, *it+1, *jt+1, r);
         }
-
-        printf("%d : sai %d\n", v+1, *it+1);
     }
 
+    s[v] = 0;
     r += d[v][k];
 
-    printf("%d : r = %lld\n", v+1, r);
     return r;
 }
 
@@ -65,6 +59,7 @@ int main () {
         }
     }
 
+/*
     for (int i = 0; i < n; i++) {
         printf("g[%d] : ", i+1);
         for (vector<unsigned int>::iterator jt = g[i].begin(); jt != g[i].end(); jt++) {
@@ -72,7 +67,7 @@ int main () {
         }
         printf("\n");
     }
+*/
 
-    printf("%lld\n", contaPares(root));
+    printf("%I64d\n", contaPares(root));
 }
-
