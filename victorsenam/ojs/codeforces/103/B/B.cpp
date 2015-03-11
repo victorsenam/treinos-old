83bf5c89de3b90022513439000949c0d2eb5cd82
Victor Sena Molero <victorsenam@gmail.com>
2015-03-11 17:30:10 -0300

    lalala

--- victorsenam/ojs/codeforces/103/B/B.cpp
+++ victorsenam/ojs/codeforces/103/B/B.cpp
@@ -5,13 +5,13 @@
 
 vector<int> g[N];
 queue<int> q;
-int v[N], cyc[N];
+int v[N], u[N];
 int n, e, a, b;
 
 int main () {
     scanf("%d %d", &n, &e);
 
-    for (int i = 0; i < n; i++) v[i] = cyc[i] = 0;
+    for (int i = 0; i < n; i++) v[i] = u[i] = 0;
     for (int i = 0; i < e; i++) {
         scanf("%d %d", &a, &b);
         
@@ -24,12 +24,22 @@ int main () {
         a = q.front();
         q.pop();
 
-        v[a] = 1;
-        b = 0;
-        for (vector<int>::iterator it = g[a].begin(); it != g[a].end(); it++) {
-            if (v[*it]) c++;
-            else q.push(*it);
+        if (!v[a]) {
+            for (vector<int>::iterator it = g[a].begin(); it != g[a].end(); it++) {
+                if (!v[*it]) q.push(*it);
+            }
         }
-        if (b > 1) cyc[a] = 1;
+        v[a]++;
     }
+
+    printf("Possibilidades de Ciclos: ");
+    for (int i = 0; i < n; i++) {
+        if (v[i] > 1) printf("%d", i);
+    }
+    printf("Desconexões: ");
+    for (int i = 0; i < n; i++) {
+        if (!v[i]) printf("%d", i);
+    }
+    
+    
 }
