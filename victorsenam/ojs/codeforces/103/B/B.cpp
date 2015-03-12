c3ca1c3c4c0f764cef95c3c3ae93200bdf395c59
Victor Sena Molero <victorsenam@gmail.com>
2015-03-12 18:34:44 -0300

    FHTANG

--- victorsenam/ojs/codeforces/103/B/B.cpp
+++ victorsenam/ojs/codeforces/103/B/B.cpp
@@ -1,17 +1,44 @@
 #include <bits/stdc++.h>
-// Incompleto
+
+using namespace std;
 
 #define N 100
 
 vector<int> g[N];
 queue<int> q;
-int v[N], cyc[N];
+int v[N];
 int n, e, a, b;
+vector<int>::iterator it;
+
+int dfs (int o, int i) {
+    int res;
+
+//    printf("%d, %d\n", o+1, i+1);
+    if (v[i]) {
+        v[i] = 2;
+        return 1;
+    } else {
+        v[i] = 1;
+    }
+
+    for (vector<int>::iterator jt = g[i].begin(); jt != g[i].end(); jt++) {
+        if (*jt == o) continue;
+        res = dfs(i, *jt);
+        if (res == 1) {
+            if (v[i] == 2) return 2;
+            else {
+                v[i] = 2;
+                return 1;
+            }
+        } if (res == 2) return 2;
+    }
+    return 0;
+}
 
 int main () {
     scanf("%d %d", &n, &e);
 
-    for (int i = 0; i < n; i++) v[i] = cyc[i] = 0;
+    for (int i = 0; i < n; i++) v[i] = 0;
     for (int i = 0; i < e; i++) {
         scanf("%d %d", &a, &b);
         
@@ -19,17 +46,64 @@ int main () {
         g[b].push_back(a);
     }
 
-    q.push(0);
+/* 
+    for (int i = 0; i < n; i++) {
+        printf("%d : ", i+1);
+        for (vector<int>::iterator it = g[i].begin(); it != g[i].end(); it++) {
+            printf("%d ", *it+1);
+        }
+        printf("\n");
+    }
+*/
+
+    dfs(0, 0);
+
+    for (int i = 0; i < n; i++) {
+        if (v[i] == 2) {
+            q.push(i);
+            v[i] = -2;
+        } else v[i] = -1;
+    }
+
     while (!q.empty()) {
         a = q.front();
         q.pop();
 
-        v[a] = 1;
         b = 0;
-        for (vector<int>::iterator it = g[a].begin(); it != g[a].end(); it++) {
-            if (v[*it]) c++;
-            else q.push(*it);
+
+//        printf("%d : ", a+1);
+
+        for (it = g[a].begin(); it != g[a].end(); it++) {
+  //          printf("%d[%d] ", *it+1, v[*it]);
+            if (v[*it] == -2) {
+                if (++b > 2) {
+    //                printf("Double Cycle");
+                    break;
+                } else {
+                    continue;
+                }
+            } else if (v[*it] >= 0) {
+                if (v[a] != *it) {
+      //              printf("Cycle");
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            
+            v[*it] = a;
+            q.push(*it);
         }
-        if (b > 1) cyc[a] = 1;
+ //       printf("\n");
+
+        if (it != g[a].end()) {
+            printf("NO\n");
+            return 0;
+        }
     }
+
+    for (a = 0; a < n && v[a] != -1; a++);
+    
+    if (a < n) printf("NO\n");
+    else printf("FHTAGN!\n");
 }
