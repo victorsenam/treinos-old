36c77ad893cbcaac6f206acf566b404708868d75
Victor Sena Molero <victorsenam@gmail.com>
2015-03-19 23:06:43 -0300

    lala

--- victorsenam/ojs/icpc/4195/4195.cpp
+++ victorsenam/ojs/icpc/4195/4195.cpp
@@ -2,35 +2,35 @@
 
 using namespace std;
 
-#define N 1000
+#define N 1002
 
 int n, m;
-unsigned int memo[N][N], s[N], aux;
+unsigned int memo[N][N], memo2[N][N], s[N], aux;
 
 unsigned int calc (int a, int b) {
-    int r = 0;
-    for (int i = a; i < b; i++) {
-        r += (s[i]-s[i+1])*(s[i+1]-s[b+1]);
+    if (memo2[a][b] == ~0u) {
+        memo2[a][b] = 0;
+        for (int i = a; i < b; i++) {
+            memo2[a][b] += (s[i]-s[i+1])*(s[i+1]-s[b+1]);
+        }
+         //   printf("[%d,%d] = %d\n", a, b, r);
     }
-    printf("[%d,%d] = %d\n", a, b, r);
-    return r;
+    return memo2[a][b];
 }
 
 unsigned int pd (int i, int k) {
-    int r, m;
+    int r;
     if (i >= n) return 0;
-    if (memo[i][k] != ~0u);
+    else if (memo[i][k] < ~0u);
+    else if (k >= n-i-1) return 0;
     else if (!k) memo[i][k] = calc(i, n);
     else {
-        m = ~0u;
-        for (int j = i; j < n; j++) {
+        for (int j = i; j <= n; j++) {
             r = calc(i, j) + pd(j+1, k-1);
-            if (r < m) m = r;
+            if (memo[i][k] > r) memo[i][k] = r;
         }
-        memo[i][k] = m;
     }
 
-    printf("(%d,%d) = %u\n", i, k, memo[i][k]);
     return memo[i][k];
 }
 
@@ -42,7 +42,10 @@ int main () {
             aux += s[n-i];
             s[n-1-i] = aux;
             for (int j = 0; j <= m; j++) memo[i][j] = ~0u;
+            for (int j = 0; j <= n; j++) memo2[i][j] = ~0u;
+            memo2[n][i] = ~0u;
         }
+        memo2[n][n] = ~0u;
 
         printf("%u\n", pd(0, m));
     }
