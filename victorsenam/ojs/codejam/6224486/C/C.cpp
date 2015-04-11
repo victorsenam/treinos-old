169aba5133902d89e0accfd20039b80030151067
Victor Sena Molero <victorsenam@gmail.com>
2015-04-11 11:14:55 -0300

    Duas respostas juntas

--- victorsenam/ojs/codejam/6224486/C/C.cpp
+++ victorsenam/ojs/codejam/6224486/C/C.cpp
@@ -4,95 +4,124 @@ using namespace std;
 
 #define L 10002
 
+struct quatern {
+    char v;
+    bool s;
+};
 
 char mul[4][4] = { {'h', 'i', 'j', 'k'}, {'i', 'h', 'k', 'j'}, {'j', 'k', 'h', 'i'}, {'k', 'j', 'i', 'h'} };
 bool sig[4][4] = { {0, 0, 0, 0}, {0, 1, 0, 1}, {0, 1, 1, 0}, {0, 0, 1, 1} };
 
-inline char inc(char a, char b, bool *ini) {
-    *ini = (sig[a-'h'][b-'h']+*ini)%2;
-    return mul[a-'h'][b-'h'];
+inline quatern inc(quatern a, quatern b) {
+    quatern c;
+    c.v = mul[a.v-'h'][b.v-'h'];
+    c.s = (a.s+b.s+sig[a.v-'h'][b.v-'h'])%2;
+    return c;
 }
-char remd(char a, char b, bool *ini) { // c*b = a
+
+quatern remd(quatern a, quatern b) { // c*b = a
     int i;
+    quatern c;
     for (i = 0; i < 4; i++) {
-        if (mul[i][b-'h'] == a) break;
+        if (mul[i][b.v-'h'] == a.v) {
+            c.v = i+'h';
+            c.s = (a.s + b.s + sig[i][b.v-'h'])%2;
+        }
     }
-    *ini = (sig[i][b-'h']+*ini)%2;
-    return i+'h';
+    return c;
 }
-char reme(char a, char b, bool *ini) { // b*c = a
+quatern reme(quatern a, quatern b) { // b*c = a
     int i;
+    quatern c;
     for (i = 0; i < 4; i++) {
-        if (mul[b-'h'][i] == a) break;
+        if (mul[b.v-'h'][i] == a.v) {
+            c.v = i+'h';
+            c.s = (a.s + b.s + sig[b.v-'h'][i])%2;
+        }
     }
-    *ini = (sig[b-'h'][i]+*ini)%2;
-    return i+'h';
+    return c;
 }
-void printState(int i, int j, char lo, bool ls, char mid, bool ms, char hi, bool hs) {
+void printState(int i, int j, quatern lo, quatern mid, quatern hi) {
     printf("(%d,%d) -> ", i, j);
-    if (ls) printf("-");
-    printf("%c ", lo);
-    if (ms) printf("-");
-    printf("%c ", mid);
-    if (hs) printf("-");
-    printf("%c ", hi);
+    if (lo.s) printf("-");
+    printf("%c ", lo.v);
+    if (mid.s) printf("-");
+    printf("%c ", mid.v);
+    if (hi.s) printf("-");
+    printf("%c ", hi.v);
     printf("\n");
 }
 
 int main () {
     unsigned long long x, l, i, j;
-    char s[L], lo, mid, hi, tot;
-    bool ls, ms, hs, ts;
+    quatern s[L], lo, mid, hi, tot;
+    char temp;
     int t;
 
     scanf("%d", &t);
     for (int c = 1; c <= t; c++) {
         scanf("%llu %llu", &l, &x);
-        scanf(" %s", s);
 
         i = 0;
         j = l*x;
-        lo = hi = mid = 'h';
-        ls = hs = ms = 0;
+        lo.v = mid.v = hi.v = 'h';
+        lo.s = mid.s = hi.s = 0;
 
         for (int k = 0; k < l; k++) {
-            mid = inc(mid, s[k], &ms);
+            scanf(" %c", &temp);
+            s[k].v = temp;
+            s[k].s = 0;
+            mid = inc(mid, s[k]);
         }
         
         tot = mid;
-        ts = ms;
 
         // exp log
         while (x > 1) {
             if (x % 2) {
-                ms = (ms+ts)%2;
-                mid = inc(mid, tot, &ms);
+                mid = inc(mid, tot);
                 x--;
             } else {
-                ms = 0;
-                mid = inc(mid, mid, &ms);
+                mid = inc(mid, mid);
                 x /= 2;
             }
         }
 
-        printf("%c %d\n", mid, ms);
-       // printState(i, j, lo, ls, mid, ms, hi, hs);
+        bool ok = 0;
+        for (i = 0; i < x*l && !ok; i++) {
+            lo = inc(lo, s[i%l]);
+            tot = reme(tot, s[i%l]);
+            if (lo.v != 'i' || lo.s) continue;
+
+            mid = tot;
+            hi = 'h';
+            for (j = x*l-1; j >= i; j--) {
+                hi = inc(s[j%l], hi);
+                mid = remd(mid, s[j%l]);
+                if (hi.v == 'k' && !hi.s && mid.v == 'j' && !mid.s) {
+                    ok = 1;
+                    break;
+                }
+            }
+        }
+
+ //       printState(i, j, lo, mid, hi);
         while (i < j) {
             do {
-                lo = inc(lo, s[i%l], &ls);
-                mid = reme(mid, s[i%l], &ms);
+                lo = inc(lo, s[i%l]);
+                mid = reme(mid, s[i%l]);
                 i++;
-     //           printState(i, j, lo, ls, mid, ms, hi, hs);
-            } while (i < j && (lo != 'i' || ls));
-            if (i < j && hi == 'k' && !hs && mid == 'j' && !ms) break; 
+   //             printState(i, j, lo, mid, hi);
+            } while (i < j && (lo.v != 'i' || lo.s));
+            if (i < j && hi.v == 'k' && !hi.s && mid.v == 'j' && !mid.s) break; 
 
             do {
                 j--;
-                hi = inc(s[j%l], hi, &hs);
-                mid = remd(mid, s[j%l], &ms);
-   //             printState(i, j, lo, ls, mid, ms, hi, hs);
-            } while (i < j && (hi != 'k' || hs));
-            if (i < j && mid == 'j' && !ms) break; 
+                hi = inc(s[j%l], hi);
+                mid = remd(mid, s[j%l]);
+     //               printState(i, j, lo, mid, hi);
+            } while (i < j && (hi.v != 'k' || hi.s));
+            if (i < j && mid.v == 'j' && !mid.s) break; 
         }
         printf("Case #%d: ", c);
         if (i < j) printf("YES");
