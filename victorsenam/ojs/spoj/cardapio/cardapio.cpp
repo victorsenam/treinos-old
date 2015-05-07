9b42d965e4274cdde966c2bdde5f106fa6555bb8
Victor Sena Molero <victorsenam@gmail.com>
2015-05-07 20:37:19 -0300

    Ahhhhgit rm teste  Malditoooos!

--- victorsenam/ojs/spoj/cardapio/cardapio.cpp
+++ victorsenam/ojs/spoj/cardapio/cardapio.cpp
@@ -2,100 +2,122 @@
 
 using namespace std;
 
-#define N 4004
+#define N 4002
 
 typedef int num;
 
 map<string, int> mp;
-map<string, int>::iterator it;
 stack<int> st;
-int cnt;
-bool val;
-char temp[22];
 vector<int> adj[N];
-int t;
-int n;
-int a, b;
-int o[N], ret[N];
 set<int> seen;
-bool isReq;
+int a, b, t, n;
+char temp[22];
+bool val;
+int ord[N], ret[N];
+int cnt;
 
-bool dfs (int v) {
-    if (o[v] != -1) return 1;
-
-    //printf("[%d]\n", v);
-    o[v] = ret[v] = cnt++;
-    st.push(v);
-
-    for (int i = 0; i < adj[v].size(); i++) {
-        if (o[adj[v][i]] != -1) {
-            ret[v] = min(ret[v], o[adj[v][i]]);
-        } else {
-            if (!dfs(adj[v][i])) return 0;
-            ret[v] = min(ret[v], ret[adj[v][i]]);
-        }
-    }
-
-    if (ret[v] < o[v]) return 1;
-
+bool checkStrong() 
+{
+    int u;
     seen.clear();
-    while (!st.empty()) {
-        a = st.top();
-        printf("%d ", a);
+
+    while (!st.empty()) 
+    {
+        u = st.top();
         st.pop();
-        if (seen.find((a+2*n)%(4*n)) != seen.end()) return 0;
-        seen.insert(a);
+        if (u%2) printf("!");
+        printf("%d ", u/2);
+        seen.insert(u);
+        ord[u] = INT_MAX;
+        if (seen.find(u^1) != seen.end()) return 0;
     }
     printf("\n");
 
     return 1;
 }
 
-int main () {
+bool dfs(int v) 
+{
+    if (ord[v] != -1) return 1;
+ //       printf("==");
+ //       if (v%2) printf("!");
+ //       printf("%d\n", v/2);
+    ord[v] = ret[v] = cnt++;
+
+    for (int i = 0; i < adj[v].size(); i++) 
+    {
+        if (ord[adj[v][i]] != -1)
+        {
+            ret[v] = min(ret[v], ord[adj[v][i]]);
+        }
+        else 
+        {
+            if(!dfs(adj[v][i])) return 0;
+            ret[v] = min(ret[v], ret[adj[v][i]]);
+        }
+    }
+
+    st.push(v);
+
+    if (ret[v] < ord[v]) return 1;
+    return checkStrong();
+}
+
+int main () 
+{
     t = 0;
-    while (scanf("%d", &n) != EOF)  {
-        for (int i = 0; i < 4*n; i++) {
-            adj[i].clear();
-            o[i] = -1;
-        }
-        while (!st.empty()) st.pop();
-
+    while (scanf("%d", &n) != EOF) 
+    {
+        t++;
         mp.clear();
-        for (int i = 0; i < n; i++) {
-            scanf(" %s", temp);
-            val = (temp[0] != '!');
-            a = mp.insert(make_pair(temp+(!val), mp.size())).first->second + (!val)*2*n;
+        while (!st.empty()) st.pop();
+        cnt = 0;
+        
+        for (int i = 0; i < n; i++) 
+        {
+            adj[4*i].clear();
+            adj[4*i+1].clear();
+            adj[4*i+2].clear();
+            adj[4*i+3].clear();
+            ord[4*i] = ord[4*i+1] = ord[4*i+2] = ord[4*i+3] = -1;
 
             scanf(" %s", temp);
             val = (temp[0] != '!');
-            b = mp.insert(make_pair(temp+(!val), mp.size())).first->second + (!val)*2*n;
- //           printf("(+) %d -> %d\n", (a+2*n)%(4*n), b);
- //           printf("(+) %d -> %d\n", (b+2*n)%(4*n), a);
+            a = mp.insert(make_pair(temp+!val, mp.size())).first->second;
+            a = 2*a + !val;
 
-            adj[(a+2*n)%(4*n)].push_back(b);
-            adj[(b+2*n)%(4*n)].push_back(a);
+            scanf(" %s", temp);
+            val = (temp[0] != '!');
+            b = mp.insert(make_pair(temp+!val, mp.size())).first->second;
+            b = 2*b + !val;
+
+            adj[a^1].push_back(b);
+            adj[b^1].push_back(a);
         }
+        adj[2*mp.size()].clear();
+        for (int i = 0; i < mp.size(); i++) adj[2*mp.size()].push_back(i);
 
-  //      for (it = mp.begin(); it != mp.end(); it++)
-  //          printf("%s %d\n", it->first.c_str(), it->second);
-  /*
-        for (int i = 0; i < 4*n; i++) {
-            printf("%d -> ", i);
-            for (int j = 0; j < adj[i].size(); j++) {
-                printf("%d ", adj[i][j]);
+        printf("Instancia %d\n", t);
+
+        if (dfs(2*mp.size())) printf("sim\n");
+        else printf("nao\n");
+        printf("\n");
+/*
+        for (map<string, int>::iterator it = mp.begin(); it != mp.end(); it++) {
+            printf("%d -> ", it->second);
+            int i = it->second;
+            for (int j = 0; j < adj[2*i].size(); j++) {
+                if (adj[2*i][j]%2) printf("!");
+                printf("%d ", adj[2*i][j]/2);
+            }
+            printf("\n");
+            printf("!%d -> ", it->second);
+            for (int j = 0; j < adj[2*i+1].size(); j++) {
+                if (adj[2*i+1][j]%2) printf("!");
+                printf("%d ", adj[2*i+1][j]/2);
             }
             printf("\n");
         }
 */
-
-        printf("Instancia %d\n", ++t);
-        printf("%d elementos\n", 2*n);
-        bool isvalid = 1;
-        for (int i = 0; i < 4*n && isvalid; i++) {
-            if (adj[i].size()) isvalid = isvalid && dfs(i);
-        }
-        if (isvalid) printf("sim\n");
-        else printf("nao\n");
-        printf("\n");
     }
 }
