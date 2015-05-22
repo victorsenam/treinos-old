fd723c5614208a838b28e3a373ba299c49cbee17
Victor Sena Molero <victorsenam@gmail.com>
2015-05-21 21:05:30 -0300

    Quando eu percebi qual era o meu erro no backtracking e parei de montar a pd

--- victorsenam/ojs/spoj/dtt/dtt.cpp
+++ victorsenam/ojs/spoj/dtt/dtt.cpp
@@ -3,51 +3,57 @@
 using namespace std;
 
 #define N 13
-#define M 1594323
-#define T 300
 
 int n;
-int a, c[2][N], equi[2], reg[2], maxTri[2], pot3[N+1];
-int memo[M][T];
-vector<int> trian;
+int a, c[2][N], equi[2], reg[2], maxTri[2];
+vector<pair<pair<int, int>, int> > trian;
 
-int pd (int state, int i, int maxTri) {
+int buildTri(int i, int * freq, int maxTri) {
     if (maxTri <= 0) return 0;
-    if (memo[state][i] != -1) return memo[state][i];
 
-    memo[state][i] = max(0, pd(state, i+1, maxTri));
+    int maxVal = 0;
+    for (int j = i; j < trian.size(); j++) {
+        if (freq[trian[j].first.first]) 
+            freq[trian[j].first.first]--;
+        else 
+            continue;
 
-    int k;
-    for (k = 0; k < 13; k++)
-        if ((trian[i]%pot3[k+1])/pot3[k] < (state%pot3[k+1])/pot3[k])
-            break;
+        if (freq[trian[j].first.second]) 
+            freq[trian[j].first.second]--;
+        else {
+            freq[trian[j].first.first]++;
+            continue;
+        }
 
-    if (k >= 13)
-        memo[state][i] = max(memo[state][i], pd(state-trian[i], i+1, maxTri-1) + 1);
+        if (freq[trian[j].second]) 
+            freq[trian[j].second]--;
+        else {
+            freq[trian[j].first.first]++;
+            freq[trian[j].first.second]++;
+            continue;
+        }
+/*
+        printf("%d(%d) %d(%d) %d(%d)\n", 
+            trian[j].first.first+1, freq[trian[j].first.first],
+            trian[j].first.second+1, freq[trian[j].first.second],
+            trian[j].second+1, freq[trian[j].second]
+        );
+*/
 
-    return memo[state][i];
+
+        maxVal = max(maxVal, buildTri(j, freq, maxTri-1) + 1);
+
+        freq[trian[j].first.first]++;
+        freq[trian[j].first.second]++;
+        freq[trian[j].second]++;
+
+        if (maxVal == maxTri) return maxVal;
+    }
+
+    return maxVal;
 }
 
 int main () {
-    pot3[0] = 1;
-    for (int i = 1; i < 14; i++) 
-        pot3[i] = pot3[i-1]*3;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = i; j < N; j++) {
-            for (int k = j; k < N; k++) {
-                if (i == j && j == k) continue;
-                if (k >= j + i + 1) continue;
-
-                trian.push_back(pot3[i] + pot3[j] + pot3[k]);
-            }
-        }
-    }
-
-    for (int i = 0; i < M; i++)
-        for (int j = 0; j < trian.size(); j++)
-            memo[i][j] = -1;
-
     while (scanf("%d", &n) != EOF && n) {
         for (int i = 0; i < N; i++) c[0][i] = c[1][i] = 0;
         for (int i = 0; i < n; i++) {
@@ -63,22 +69,32 @@ int main () {
             c[1][i] %= 3;
         }
 
-        printf("fp\n");
         if (equi[0] > equi[1]) printf("1\n");
         else if (equi[0] < equi[1]) printf("2\n");
         else {
-            int a, b;
+            if (trian.size() == 0) {
+                for (int i = 0; i < N; i++)
+                    for (int j = i; j < N; j++)
+                        for (int k = j; k < N; k++) {
+                            if (i == j && j == k) continue;
+                            if (k >= j + i + 1) continue;
+
+                            trian.push_back(make_pair(make_pair(i, j), k));
+                        }
+            }
 
             maxTri[0] = maxTri[1] = 0;
             for (int i = 0; i < N; i++) {
                 maxTri[0] += c[0][i];
                 maxTri[1] += c[1][i];
-                a += c[0][i]*pot3[i];
-                b += c[1][i]*pot3[i];
             }
 
-            reg[0] = pd(a, 0, maxTri[0]/3);
-            reg[1] = pd(b, 0, maxTri[1]/3);
+//printf("f0\n");
+            reg[0] = buildTri(0, c[0], maxTri[0]/3);
+//printf("f1\n");
+            reg[1] = buildTri(0, c[1], maxTri[1]/3);
+
+//            printf("%d %d\n", reg[0], reg[1]);
 
             if (reg[0] > reg[1]) printf("1\n");
             else if (reg[0] < reg[1]) printf("2\n");
@@ -86,3 +102,4 @@ int main () {
         }
     }
 }
+
