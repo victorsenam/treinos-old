d6e4a7350350e037ba0559d6b6f2beac5b4041aa
Victor Sena Molero <victorsenam@gmail.com>
2015-05-23 10:49:55 -0300

    Mudando um pouco o template

--- victorsenam/temp.cpp
+++ victorsenam/temp.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 #ifndef ONLINE_JUDGE
-#define debug(...) fprintf(stderr, "%d| ", __LINE__); fprintf(stderr, __VA_ARGS__)
+#define debug(...) fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__)
 #else
 #define debug(...) //
 #endif
