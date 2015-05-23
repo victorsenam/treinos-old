c7552ad6aeb25a2ed1d140bededebbe53cc935fc
Victor Sena Molero <victorsenam@gmail.com>
2015-05-23 11:42:51 -0300

    Temp maneiro

--- victorsenam/temp.cpp
+++ victorsenam/temp.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+typedef int num;
 #ifndef ONLINE_JUDGE
 #define debug(...) fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__)
 #else
 #define debug(...) //
 #endif
 
+
 #define N 
 
-using namespace std;
-
-typedef int num;
+int n;
 
 int main () {
 }
