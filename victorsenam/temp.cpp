765db3c479b5579c7301061427f771f71e1ce969
Victor Sena Molero <victorsenam@gmail.com>
2015-05-24 23:15:29 -0300

    Colocando um break line maroto e sexy no final do debug

--- victorsenam/temp.cpp
+++ victorsenam/temp.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 typedef int num;
 #ifndef ONLINE_JUDGE
-#define debug(...) fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__)
+#define debug(...) fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #else
 #define debug(...) //
 #endif
