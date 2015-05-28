44488d265af717a7ec4d28e42bc93660abfcdbda
Victor Sena Molero <victorsenam@gmail.com>
2015-05-28 20:27:59 -0300

    lala

--- victorsenam/ojs/spoj/banfarao/banfarao.cpp
+++ victorsenam/ojs/spoj/banfarao/banfarao.cpp
@@ -24,6 +24,9 @@ void joinSols(int to[3][2], int fl[3][2], int fr[3][2]) {
         to[0][1] = fr[0][1];
 
     // da direita
+    debug("to [%d,%d) [%d,%d) [%d,%d)", to[0][0], to[0][1], to[1][0], to[1][1], to[2][0], to[2][1]);
+    debug("fl [%d,%d) [%d,%d) [%d,%d)", fl[0][0], fl[0][1], fl[1][0], fl[1][1], fl[2][0], fl[2][1]);
+    debug("fr [%d,%d) [%d,%d) [%d,%d)", fr[0][0], fr[0][1], fr[1][0], fr[1][1], fr[2][0], fr[2][1]);
     to[2][1] = fr[2][1];
     to[2][0] = fr[2][0];
     if (acc[fl[2][0]] < acc[to[2][0]])
@@ -68,23 +71,35 @@ void setInit(int root, int ind, int lo, int hi) {
     }
 }
 
-void getInt(int ans[3][2], int root, int ql, int qr, int lo, int hi) {
-    if (lo >= ql && hi <= qr) ans = seg[root];
-    if (lo >= qr || hi <= ql) ans = NULL;
+bool getInt(int ans[3][2], int root, int ql, int qr, int lo, int hi) {
+    debug("[%d,%d) -> %d [%d,%d)", ql, qr, root, lo, hi);
+    if (lo >= ql && hi <= qr) {
+        ans = seg[root];
+        debug("seg[root] [%d,%d) [%d,%d) [%d,%d)", seg[root][0][0], seg[root][0][1], seg[root][1][0], seg[root][1][1], seg[root][2][0], seg[root][2][1]);
+        debug("ans [%d,%d) [%d,%d) [%d,%d)", ans[0][0], ans[0][1], ans[1][0], ans[1][1], ans[2][0], ans[2][1]);
+        return 1;
+    } else if (lo >= qr || hi <= ql) {
+        debug("fail on %d", root);
+        ans = NULL;
+        return 0;
+    } else {
+        int al[3][2], ar[3][2];
+        bool cl, cr;
+        
+        int mid = lo + (hi-lo)/2;
+        cl = getInt(al, root*2, ql, qr, lo, mid);
+        cr = getInt(ar, root*2+1, ql, qr, mid, hi);
+        debug("got %d %d on %d", cl, cr, root);
 
-    int al[3][2];
-    int ar[3][2];
-    
-    int mid = lo + (hi-lo)/2;
-    getInt(al, root*2, ql, qr, lo, mid);
-    getInt(ar, root*2+1, ql, qr, mid, hi);
+        if (!cl)
+            ans = ar;
+        else if (!cr)
+            ans = al;
+        else 
+            joinSols(ans, al, ar);
 
-    if (al == NULL)
-        ans = ar;
-    else if (ar == NULL)
-        ans = al;
-    else 
-        joinSols(ans, al, ar);
+        return 1;
+    }
 }
 
 int main () {
@@ -105,7 +120,7 @@ int main () {
         int lala[3][2];
         for (int i = 0; i < q; i++) {
             scanf("%d %d", &a, &b);
-            getInt(lala, 1, a, b, 0, n);
+            getInt(lala, 1, a-1, b, 0, n);
             printf("%d %d\n", acc[lala[1][1]] - acc[lala[1][0]], lala[1][1] - lala[1][0]);
         }
     }
