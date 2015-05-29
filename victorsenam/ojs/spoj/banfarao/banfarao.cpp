62e09b548843826a31fc808aff792ffa4a882dd2
Victor Sena Molero <victorsenam@gmail.com>
2015-05-29 12:53:01 -0300

    Pronto pra jogar o codigo fora e começar de novo

--- victorsenam/ojs/spoj/banfarao/banfarao.cpp
+++ victorsenam/ojs/spoj/banfarao/banfarao.cpp
@@ -2,15 +2,16 @@
 
 using namespace std;
 typedef int num;
+#define ONLINE_JUDGE
 #ifndef ONLINE_JUDGE
 #define debug(...) fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #else
 #define debug(...) //
 #endif
 
-#define N 100000
+#define N 200000
 
-num seg[4*N+1][3][2];
+num seg[4*N][3][2];
 int n, q, t, a, b;
 int acc[N];
 
@@ -19,39 +20,54 @@ void joinSols(int to[3][2], int fl[3][2], int fr[3][2]) {
     
     // da esquerda
     to[0][0] = fl[0][0];
-    to[0][0] = fl[0][1];
-    if (acc[fr[0][1]] > acc[to[0][1]])
+    if (acc[fr[0][1]] >= acc[fl[0][1]])
         to[0][1] = fr[0][1];
+    else
+        to[0][1] = fl[0][1];
+
 
     // da direita
-    debug("to [%d,%d) [%d,%d) [%d,%d)", to[0][0], to[0][1], to[1][0], to[1][1], to[2][0], to[2][1]);
-    debug("fl [%d,%d) [%d,%d) [%d,%d)", fl[0][0], fl[0][1], fl[1][0], fl[1][1], fl[2][0], fl[2][1]);
-    debug("fr [%d,%d) [%d,%d) [%d,%d)", fr[0][0], fr[0][1], fr[1][0], fr[1][1], fr[2][0], fr[2][1]);
     to[2][1] = fr[2][1];
-    to[2][0] = fr[2][0];
-    if (acc[fl[2][0]] < acc[to[2][0]])
+    if (acc[fl[2][0]] <= acc[fr[2][0]])
         to[2][0] = fl[2][0];
+    else
+        to[2][0] = fr[2][0];
+
 
     // do centro
-    int mend, mini;
-    mend = fl[1][1];
-    if (acc[fr[0][1]] > acc[mend])
-        mend = fr[0][1];
-    mini = fr[1][0];
-    if (acc[fl[2][0]] < mini)
-        mini = fl[2][0];
+    int lini, lend, rini, rend;
+    lini = fl[1][0];
+    if (acc[fl[1][1]] > acc[fr[0][1]])
+        lend = fl[1][1];
+    else
+        lend = fr[0][1];
 
-    if (acc[mend] - acc[fl[1][0]] > acc[fr[1][1]] - acc[mini])
-        mini = fl[1][0];
-    else 
-        mend = fr[1][1];
+    rend = fr[1][1];
+    if (acc[fl[2][0]] <= acc[fr[1][0]])
+        rini = fl[2][0];
+    else
+        rini = fr[1][0];
 
-    to[1][0] = mini;
-    to[1][1] = mend;
+    if (acc[lend] - acc[lini] > acc[rend] - acc[rini]) {
+        to[1][0] = lini;
+        to[1][1] = lend;
+    } else if (acc[lend] - acc[lini] < acc[rend] - acc[rini]) {
+        to[1][0] = rini;
+        to[1][1] = rend;
+    } else if (lend - lini > rend - rini) {
+        to[1][0] = lini;
+        to[1][1] = lend;
+    } else {
+        to[1][0] = rini;
+        to[1][1] = rend;
+    }
+
+    debug("fl [%d,%d) [%d,%d) [%d,%d)", fl[0][0], fl[0][1], fl[1][0], fl[1][1], fl[2][0], fl[2][1]);
+    debug("fr [%d,%d) [%d,%d) [%d,%d)", fr[0][0], fr[0][1], fr[1][0], fr[1][1], fr[2][0], fr[2][1]);
+    debug("to [%d,%d) [%d,%d) [%d,%d)", to[0][0], to[0][1], to[1][0], to[1][1], to[2][0], to[2][1]);
 }
 
 void setInit(int root, int ind, int lo, int hi) {
-    debug("%d %d %d %d", root, ind, lo, hi);
     if (lo + 1 >= hi) {
         seg[root][0][0] = seg[root][1][0] = seg[root][2][0] = ind;
         seg[root][0][1] = seg[root][1][1] = seg[root][2][1] = ind+1;
@@ -67,16 +83,21 @@ void setInit(int root, int ind, int lo, int hi) {
         else
             setInit(2*root+1, ind, mid, hi);
         
+        debug("joining [%d,%d) from [%d,%d) and [%d,%d)", lo, hi, lo, mid, mid, hi);
         joinSols(seg[root], seg[root*2], seg[root*2+1]);
     }
 }
 
+void copyAns(int to[3][2], int from[3][2]) {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 2; j++)
+            to[i][j] = from[i][j];
+}
+
 bool getInt(int ans[3][2], int root, int ql, int qr, int lo, int hi) {
-    debug("[%d,%d) -> %d [%d,%d)", ql, qr, root, lo, hi);
     if (lo >= ql && hi <= qr) {
-        ans = seg[root];
-        debug("seg[root] [%d,%d) [%d,%d) [%d,%d)", seg[root][0][0], seg[root][0][1], seg[root][1][0], seg[root][1][1], seg[root][2][0], seg[root][2][1]);
-        debug("ans [%d,%d) [%d,%d) [%d,%d)", ans[0][0], ans[0][1], ans[1][0], ans[1][1], ans[2][0], ans[2][1]);
+        copyAns(ans, seg[root]);
+        debug("ans[%d,%d) = seg[%d] = [%d,%d) [%d,%d) [%d,%d)", lo, hi, root, ans[0][0], ans[0][1], ans[1][0], ans[1][1], ans[2][0], ans[2][1]);
         return 1;
     } else if (lo >= qr || hi <= ql) {
         debug("fail on %d", root);
@@ -89,14 +110,19 @@ bool getInt(int ans[3][2], int root, int ql, int qr, int lo, int hi) {
         int mid = lo + (hi-lo)/2;
         cl = getInt(al, root*2, ql, qr, lo, mid);
         cr = getInt(ar, root*2+1, ql, qr, mid, hi);
-        debug("got %d %d on %d", cl, cr, root);
 
-        if (!cl)
-            ans = ar;
-        else if (!cr)
-            ans = al;
-        else 
+        if (!cl) {
+            debug("[%d,%d) is [%d,%d)", lo, hi, mid, hi);
+            copyAns(ans, ar);
+        } else if (!cr) {
+            debug("[%d,%d) is [%d,%d)", lo, hi, lo, mid);
+            copyAns(ans, al);
+        } else {
+            debug("joining [%d,%d) from [%d,%d) and [%d,%d)", lo, hi, lo, mid, mid, hi);
             joinSols(ans, al, ar);
+        }
+
+        debug("ans[%d,%d) = [%d,%d) [%d,%d) [%d,%d)", lo, hi, ans[0][0], ans[0][1], ans[1][0], ans[1][1], ans[2][0], ans[2][1]);
 
         return 1;
     }
@@ -106,8 +132,8 @@ int main () {
     scanf("%d", &t);
     while (t--) {
         scanf("%d", &n);
-        scanf("%d", &acc[0]);
-        for (int i = 1; i < n; i++) {
+        acc[0] = 0;
+        for (int i = 1; i <= n; i++) {
             scanf("%d", &acc[i]);
             acc[i] += acc[i-1];
         }
@@ -120,6 +146,7 @@ int main () {
         int lala[3][2];
         for (int i = 0; i < q; i++) {
             scanf("%d %d", &a, &b);
+            debug("Query [%d,%d)", a-1, b);
             getInt(lala, 1, a-1, b, 0, n);
             printf("%d %d\n", acc[lala[1][1]] - acc[lala[1][0]], lala[1][1] - lala[1][0]);
         }
