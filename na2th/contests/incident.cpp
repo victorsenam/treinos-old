644fcece74168d6ac8794d22964806865c44fb6f
nathanPro <nathan@vieiraproenca.com>
2015-06-05 02:15:37 +0000

    EU MONTO O GRAFO

--- na2th/contests/incident.cpp
+++ na2th/contests/incident.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 typedef int num;
 typedef int point;
-typedef pair< pair<long long, int>, int > state;
+
 num x[MP], y[MP];
 point pend[MP], anchor, doc, por;
 int ps, pds;
 num t, m, c, d[MP];
+bool done[MP];
+
 vector< pair<int, long long> > adj[MP];
-// Pontos guardados
-// [0..c-1] cabines
-// [c..c+2m] murais
 
 inline int cP(point a, point b, point c)
 {
@@ -32,10 +31,10 @@ inline long long dist(point a, point b)
     return (x[a]-x[b])*(x[a]-x[b]) + (y[a]-y[b])*(y[a]-y[b]);
 }
 
-inline bool improperInt( point a, point b, point c, point d )
+inline bool properInt( point a, point b, point c, point d )
 {
-    return ( sign(cP(a,b,c))*sign(cP(a,b,d)) <= 0 &&
-             sign(cP(c,d,a))*sign(cP(c,d,b)) <= 0);
+    return ( sign(cP(a,b,c))*sign(cP(a,b,d)) < 0 &&
+             sign(cP(c,d,a))*sign(cP(c,d,b)) < 0);
 }
 
 inline bool lexicalLess(point a,point b)
@@ -47,40 +46,15 @@ inline bool lexicalLess(point a,point b)
 inline bool grahamLess(point a,point b)
 {
     num val = cP(anchor, a, b);
-    if( !val ) return x[a] < x[b];
+    if( !val ) return lexicalLess(a, b);
     return val > 0;
 }
 
-long long djs(int s, int e, int tic)
+void connect(point a, point b)
 {
-    state aux;
-    for( int i = 0; i < ps; i++ )
-        d[i] = INT_MAX;;
-    d[s] = 0;
-    priority_queue<state, vector<state>, greater<state> > pq;
-    pq.push( make_pair( make_pair(0, tic) , s));
-    while( !pq.empty() )
-    {
-        aux = pq.top(); pq.pop();
-        long long lDist = aux.first.first;
-        int tr = aux.first.second;
-        int u  = aux.second;
-        if( lDist <= d[u] )
-            for( int i = 0; i < adj[u].size(); i++ )
-            {
-                int v = adj[u][i].first;
-                if( adj[u][i].second == 0 && tr > 0 && lDist < d[v] )
-                {
-                    d[v] = lDist;
-                    pq.push( make_pair( make_pair(lDist, tr-1), v));
-                }
-                else if( lDist + adj[u][i].second < d[v] )
-                {
-                    d[v] = lDist + adj[u][i].second;
-                    pq.push( make_pair( make_pair( lDist + adj[u][i].second, tr), v) );
-                }
-            }
-    }
+    long long d = dist( a, b );
+    adj[a].push_back( make_pair(b, d) );
+    adj[b].push_back( make_pair(a, d) );
 }
 
 int main()
@@ -109,36 +83,36 @@ int main()
 
         for( int i = 0; i < ps; i++ )
             adj[i].clear();
-        
-        // Monta o grafo das caminhadas em tempo
+
         while( pds > 0 )
         {
-            // Acha ancora e remove da pendencia
             anchor = pend[0];
             int ai = 0, skipTo;
             for( int i = 1; i < pds; i++ )
                 if( lexicalLess(pend[i], anchor) )
                 {
                     anchor = pend[i];
-                    ai     = i;
+                    ai = i;
                 }
-            swap( pend[pds-1], pend[ai] ); pds--;
+            swap( pend[ai], pend[pds-1] ); pds--;
             sort( pend, pend+pds, grahamLess );
-            for( int v = 0; v < pds; v++ )
+            for( int p = 0; p < pds; p++ )
             {
-                long long d = dist( anchor, pend[v] );
-                adj[anchor].push_back( make_pair(pend[v], d) );
-                adj[pend[v]].push_back( make_pair(anchor, d) );
-
-                // Se é mural 
-                if( pend[v] > c && pend[v] < doc )
+                // Se mural
+                if( c+c%2 <= pend[p] && pend[p] < doc )
                 {
-                    skipTo = pend[v]^1;
-                    adj[anchor].push_back( make_pair(skipTo, d) );
-                    adj[skipTo].push_back( make_pair(anchor, d) );
-                    while( grahamLess(pend[v], skipTo) ) v++;
-                    if( pend[v] == skipTo ) v++;
+                    skipTo = pend[p]^1;
+                    if( anchor != skipTo )
+                        connect( anchor, skipTo );
+                    while( grahamLess( pend[p], skipTo ) )
+                    {
+                        if( !properInt(anchor, pend[p], skipTo, skipTo^1) )
+                            connect( anchor, pend[p] );
+                        p++;
+                    }
                 }
+                else
+                    connect(anchor, pend[p]);
             }
         }
 
@@ -146,16 +120,15 @@ int main()
             for( int j = i+1; j < c; j++ )
             {
                 bool free = true;
-                for( int k = c+c%1; free && k < doc; k += 2 )
-                    free = free && !improperInt(i,j,k,k+1);
+                for( int k = c+c%2; free && k < doc; k += 2 )
+                    if( properInt(i,j,k,k^1) || !cP(i,j,k) || !cP(i,j,k^1) )
+                        free = false;
                 if( free )
                 {
                     adj[i].push_back( make_pair(j, 0LL));
                     adj[j].push_back( make_pair(i, 0LL));
                 }
             }
-
-        djs(doc, por, t);
-        printf("%.1f\n", sqrt(d[por]) );
+        printf("Grafo montado.\n");
     }
 }
