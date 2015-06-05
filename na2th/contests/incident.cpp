236478b35180d5ce02280e27ee74e0a819460b63
nathanPro <nathan@vieiraproenca.com>
2015-06-05 16:08:08 +0000

    pior holandes

--- na2th/contests/incident.cpp
+++ na2th/contests/incident.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #define MP 200
+#define EPS 1e-9
 using namespace std;
 typedef int num;
 typedef int point;
@@ -7,10 +8,12 @@ typedef int point;
 num x[MP], y[MP];
 point pend[MP], anchor, doc, por;
 int ps, pds;
-num t, m, c, d[MP];
-bool done[MP];
+num t, m, c;
 
+long long int d[MP];
+int anc[MP];
 vector< pair<int, long long> > adj[MP];
+typedef pair< pair<long long, int>, point> state;
 
 inline int cP(point a, point b, point c)
 {
@@ -37,6 +40,16 @@ inline bool properInt( point a, point b, point c, point d )
              sign(cP(c,d,a))*sign(cP(c,d,b)) < 0);
 }
 
+inline bool improperInt(point a,point b, point c, point d)
+{
+    if( properInt(a,b,c,d) ) return true;
+    if( fabs(sqrt(dist(a,b)) - sqrt(dist(a,c)) - sqrt(dist(c,b))) < EPS ) return true;
+    if( fabs(sqrt(dist(a,b)) - sqrt(dist(a,d)) - sqrt(dist(d,b))) < EPS ) return true;
+    if( fabs(sqrt(dist(c,d)) - sqrt(dist(c,a)) - sqrt(dist(a,d))) < EPS ) return true;
+    if( fabs(sqrt(dist(c,d)) - sqrt(dist(c,b)) - sqrt(dist(b,d))) < EPS ) return true;
+    return false;
+}
+
 inline bool lexicalLess(point a,point b)
 {
     if( x[a] == x[b] ) return y[a] < y[b];
@@ -55,6 +68,40 @@ void connect(point a, point b)
     long long d = dist( a, b );
     adj[a].push_back( make_pair(b, d) );
     adj[b].push_back( make_pair(a, d) );
+}
+
+void djs(int s)
+{
+    for( int i = 0; i < ps; i++ )
+        d[i] = LLONG_MAX;
+    priority_queue<state, vector<state>, greater<state> > Q;
+    d[s] = 0;
+    Q.push( make_pair(make_pair(0LL, t),s) );
+    while( !Q.empty() )
+    {
+        point     v  = Q.top().second;
+        long long dv = Q.top().first.first;
+        int       tr = Q.top().first.second;
+        Q.pop();
+        if( dv == d[v] )
+        {
+            for( int i = 0; i < adj[v].size(); i++ )
+            {
+                point     u = adj[v][i].first;
+                long long w = adj[v][i].second;
+                if( w > 0 )
+                {
+                    d[u] = min( d[u], d[v] + w );
+                    Q.push( make_pair(make_pair( d[v] + w, tr ), u) );
+                }
+                else if ( tr > 0 )
+                {
+                    d[u] = min( d[u], d[v] );
+                    Q.push( make_pair(make_pair( d[v], tr-1), u) );
+                }
+            }
+        }
+    }
 }
 
 int main()
@@ -121,7 +168,7 @@ int main()
             {
                 bool free = true;
                 for( int k = c+c%2; free && k < doc; k += 2 )
-                    if( properInt(i,j,k,k^1) || !cP(i,j,k) || !cP(i,j,k^1) )
+                    if( improperInt(i,j,k,k^1) )
                         free = false;
                 if( free )
                 {
@@ -130,5 +177,7 @@ int main()
                 }
             }
         printf("Grafo montado.\n");
+        djs(doc);
+        printf("%.1f\n", sqrt(d[por]));
     }
 }
