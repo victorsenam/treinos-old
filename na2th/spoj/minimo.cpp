7e55a2ffb868d34d7d2654f3a6a9b8039dca98bb
nathanPro <nathan@vieiraproenca.com>
2015-06-13 19:17:26 +0000

    Mais grafos - bobos - sendo resolvidos

---
+++ na2th/spoj/minimo.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define oo ((int)1e9)
+#define MV 105
+using namespace std;
+typedef int num;
+num dist[MV][MV][MV+1];
+num n, m, t, c;
+
+int main()
+{
+    int tc = 1;
+    while( scanf("%d %d", &n, &m) != EOF )
+    {
+        printf("Instancia %d\n", tc++);
+        for( int i = 0; i < n; i++ )
+            for( int j = 0; j < n; j++ )
+                dist[i][j][0] = (i == j) ? 0 : oo;
+        for( int i = 0; i < m; i++ )
+        {
+            num u, v, w;
+            scanf("%d %d %d", &u, &v, &w); u--;v--;
+            dist[u][v][0] = min( dist[u][v][0], w );
+        }
+        scanf("%d", &c);
+
+        for( int k = 0; k < n; k++ )
+            for( int i = 0; i < n; i++ )
+                for( int j = 0; j < n; j++ )
+                    dist[i][j][k+1] = min( dist[i][j][k], dist[i][k][k] + dist[k][j][k] );
+
+        while( c-- > 0 )
+        {
+            num u,v,t;
+            scanf("%d %d %d", &u, &v, &t);
+            if( dist[u-1][v-1][t] < oo )
+                printf("%d\n", dist[u-1][v-1][t]);
+            else
+                printf("-1\n");
+        }
+        printf("\n");
+    }
+}
