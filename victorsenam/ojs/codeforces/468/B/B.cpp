b5fc4f51051e343ef7002293a31f2e83d06914c7
Victor Sena Molero <victorsenam@gmail.com>
2015-08-08 00:09:55 -0300

    468B GG

--- victorsenam/ojs/codeforces/468/B/B.cpp
+++ victorsenam/ojs/codeforces/468/B/B.cpp
@@ -5,97 +5,72 @@ typedef long long int ll;
 typedef unsigned long long int ull;
 typedef int num;
 #ifndef ONLINE_JUDGE
-#define debug(...) {fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n");}
+#define debug(...) fprintf(stderr, "%3d| ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
 #else
-#define debug(...) {}
+#define debug(...) //
 #endif
 
 const int N = 100007;
 
-int n, a, b;
-int v[N], pos[N];
-int sa, sb;
+int n, d[2];
+int adj[N][2];
+bool visi[N];
 bool cor[N];
-bool vis[N];
-int deg[N];
-int it;
-int res;
-vector<int> adj[N][2];
+int v[N];
+int pre[N];
+int pos[N];
 
 void dfs(int v, int c) {
-    if (v == -1 || vis[v]) {
-        if (v >= 0 && cor[v] == c)
+    if (v >= n) 
+        return;
+    if (visi[v]) {
+        if (cor[v] == c)
             return;
         printf("NO\n");
         exit(0);
     }
 
+    visi[v] = 1;
     cor[v] = c;
-    vis[v] = 1;
-    res++;
 
     for (int j = 0; j < 2; j++)
-        for (int i = 0; i < adj[v][j].size(); i++)
-            dfs(adj[v][j][i], c);
+        dfs(adj[v][j], c);
 }
 
 int main () {
-    scanf("%d %d %d", &n, &a, &b);
+    scanf("%d %d %d", &n, d, d+1);
 
     for (int i = 0; i < n; i++) {
-        scanf("%d", pos+i);
-        v[i] = pos[i];
+        scanf("%d", v+i);
+        pre[i] = v[i];
     }
 
     sort(v, v+n);
 
-    sa = -1;
-    sb = -1;
-
+    for (int i = 0; i < n; i++)
+        pos[i] = lower_bound(v, v+n, pre[i]) - v;
+    
     for (int i = 0; i < n; i++) {
-        it = lower_bound(v, v+n, a-v[i]) - v;
-        if (it == i) {
-            sa = i;
-            adj[i][0].push_back(i);
-            deg[i]++;
-        }
-        if (it < n) {
-            adj[i][0].push_back(it);
-            deg[i]++;
-        }
-
-        it = lower_bound(v, v+n, b-v[i]) - v;
-        if (it == i) {
-            sb = i;
-            adj[i][1].push_back(i);
-            deg[i]++;
-        }
-        if (it < n) {
-            adj[i][1].push_back(it);
-            deg[i]++;
+        for (int j = 0; j < 2; j++) {
+            adj[i][j] = lower_bound(v, v+n, d[j]-v[i]) - v;
+            if (adj[i][j] >= n || v[adj[i][j]] != d[j]-v[i]) 
+                adj[i][j] = n;
         }
     }
 
-    res = 0;
-    if (sa >= 0) dfs(sa, 0);
-    if (sb >= 0) dfs(sb, 1);
-
-    int ores = res;
     for (int i = 0; i < n; i++) {
-        if (deg[i] == 0 || deg[i] == 1) {
-            if (adj[i][1].size())
-                dfs(i, 0);
-            else
-                dfs(i, 1);
+        for (int j = 0; j < 2; j++) {
+            if (adj[i][j] >= n && adj[i][j^1] < n) dfs(i, j^1);
+            else if (adj[i][j] >= n && adj[i][j^1] >= n) {
+                dfs(i, j);
+                dfs(i, j^1);
+            }
         }
-
-        if ((ores - res)%2) dfs(-1, 0);
     }
 
     for (int i = 0; i < n; i++)
-        if (cor[i] == 0)
-            dfs(i, 1);
-    
+        if (!visi[i]) dfs(i, 1);
+
     printf("YES\n");
     for (int i = 0; i < n; i++)
         printf("%d ", cor[pos[i]]);
