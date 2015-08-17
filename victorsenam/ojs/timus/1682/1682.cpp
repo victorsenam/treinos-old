8dca3f1dc0855e5ab341d4437b5987a0ebdf8efb
Victor Sena Molero <victorsenam@gmail.com>
2015-08-17 19:13:45 -0300

    1682

--- victorsenam/ojs/timus/1682/1682.cpp
+++ victorsenam/ojs/timus/1682/1682.cpp
@@ -4,14 +4,10 @@ using namespace std;
 
 const int K = 100007;
 
-vector<int> v[K][2];
-int uf[2*K];
-int we[2*K];
-int k;
-
-int join(int a, int b) {
-    
-}
+vector<int> v[2][K];
+int uf[3*K];
+int we[3*K];
+int k, a[2];
 
 int find (int a) {
     if (uf[uf[a]] != uf[a])
@@ -19,9 +15,27 @@ int find (int a) {
     return uf[a];
 }
 
+int join(int a, int b) {
+    a = find(a);
+    b = find(b);
+
+    if (a == b) 
+        return a;
+    if (we[a] > we[b])
+        swap(a, b);
+    uf[a] = b;
+    we[b] += we[a];
+    return b;
+}
+
 int main () {
     scanf("%d", &k);
 
-    for (int i = 0; i <= 2*k; i++)
+    for (int i = 0; i <= 3*k; i++) {
         uf[i] = i;
+        we[i] = 1;
+    }
+
+    for (int i = 1; i <= 3*k; i++) {
+    }
 }
