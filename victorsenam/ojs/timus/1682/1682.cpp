b9ab110835c3deb22f6a781b29d6824419056604
Victor Sena Molero <victorsenam@gmail.com>
2015-08-19 13:29:55 -0300

    Desafios TImus =D

--- victorsenam/ojs/timus/1682/1682.cpp
+++ victorsenam/ojs/timus/1682/1682.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 const int K = 100007;
 
-vector<int> v[2][K];
+vector<int> nor[K], qua[K], sol;
 int uf[3*K];
 int we[3*K];
-int k, a[2];
+int k, a, b;
 
 int find (int a) {
     if (uf[uf[a]] != uf[a])
@@ -36,6 +36,26 @@ int main () {
         we[i] = 1;
     }
 
-    for (int i = 1; i <= 3*k; i++) {
+    for (long long int i = 1; i <= 3*k; i++) {
+        a = ((k-i)%k + k)%k;
+        b = ((k-(i*i)%k)%k + k)%k;
+
+        sol.clear();
+        for (int j = 0; j < nor[b].size(); j++)
+            sol.push_back(nor[b][j]);
+        for (int j = 0; j < qua[a].size(); j++)
+            sol.push_back(qua[a][j]);
+        sort(sol.begin(), sol.end());
+        int siz = unique(sol.begin(), sol.end()) - sol.begin();
+
+        for (int j = 0; j < siz; j++) {
+                if (find(i) == find(sol[j])) {
+                printf("%d\n", (int) i);
+                return 0;
+            }
+            join(i, sol[j]);
+        }
+        nor[i%k].push_back(i);
+        qua[(i*i)%k].push_back(i);
     }
 }
