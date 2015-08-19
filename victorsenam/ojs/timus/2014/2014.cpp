e03758d2521a22ec97e4d0f72a1b15248f6d4e02
Victor Sena Molero <victorsenam@gmail.com>
2015-08-19 15:17:27 -0300

    Wohol

--- victorsenam/ojs/timus/2014/2014.cpp
+++ victorsenam/ojs/timus/2014/2014.cpp
@@ -2,11 +2,13 @@
 
 using namespace std;
 
-const int N = 100007;
+const int N = 400007;
 
-struct time {
+typedef long long int ll;
+
+struct hora {
     int month, day, hour, minute;
-    bool operator < (const time & a) {
+    bool operator < (const hora & a) {
         if (month != a.month)
             return month < a.month;
         if (day != a.day)
@@ -35,20 +37,49 @@ struct seg {
     void update (int r, int p, ll val) {
         if (esq[r] > p || dir[r] <= p)
             return;
-        if (req[r] == p && dir[r] == p+1) {
+        if (esq[r] == p && dir[r] == p+1) {
             if (val > 0)
                 bal[r] = val;
             else
-                deb[r] = val;
+                deb[r] = -val;
             return;
         }
 
         update(2*r, p, val);
         update(2*r+1, p, val);
+
+        deb[r] = deb[2*r] + max(0ll, deb[2*r+1] - bal[2*r]);
+        bal[r] = bal[2*r+1] + max(0ll, bal[2*r] - deb[2*r+1]);
+		
+		//printf("[%d,%d) deb = %lld, bal = %lld\n", esq[r], dir[r], deb[r], bal[r]);
+    }
+
+    ll getDeb() {
+        return deb[1];
     }
 };
 
-set<time> s;
+hora ord[N];
+pair<ll, hora> v[N];
+hora aux;
+ll val;
+int n;
+seg tree;
 
 int main () {
+    scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%lld %d.%d %d:%d", &val, &aux.day, &aux.month, &aux.hour, &aux.minute);
+		ord[i] = aux;
+		v[i].first = val;
+		v[i].second = aux;
+	}
+	
+	sort(ord, ord+n);
+	tree.build(1, 0, n);
+	for (int i = 0; i < n; i++) {
+		tree.update(1, lower_bound(ord, ord+n, v[i].second) - ord, v[i].first);
+		printf("%lld\n", -tree.getDeb());
+	}
 }
